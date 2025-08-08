6c8d77833a3264371dc0b516ceb59ad7990e34ab
Mrwhite987 <Mrwhite987@hotmail.com>
2025-08-08 10:47:12 +0800

    Refactors grid creation and adds button styling
    Refactors grid initialization logic by extracting reusable functions for grid creation and pen effect, improving code readability and maintainability. Updates button text for clarity and enhances button and grid styling for a better user experience.

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
-let container = document.querySelector("div.container");
-for (let i = 0; i < 16 * 16; i++) {
-  let squareDiv = document.createElement("div");
-  container.appendChild(squareDiv);
+// Initialize grid.
+createGrid(16);
+addPenEffect();
+
+function addPenEffect() {
+  let allSquares = document.querySelectorAll(".container > div");
+
+  allSquares.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "#fb8500";
+    });
+    square.addEventListener("mouseleave", () => {
+      square.style.backgroundColor = "#219ebc";
+    });
+  });
 }
-
-let allSquares = document.querySelectorAll(".container > div");
-
-allSquares.forEach((square) => {
-  square.addEventListener("mouseenter", () => {
-    square.style.backgroundColor = "#fb8500";
-  });
-  square.addEventListener("mouseleave", () => {
-    square.style.backgroundColor = "#219ebc";
-  });
-});
 
 let sizeButton = document.querySelector("button");
 sizeButton.addEventListener("click", () => {
   let squareNum = prompt("Enter a new number of squares per side:");
-  if (Number(squareNum) > 100) {
+
+  createGrid(Number(squareNum));
+  addPenEffect();
+});
+
+function createGrid(squareNumPerSide) {
+  let container = document.querySelector("div.container");
+  if (squareNumPerSide > 100) {
     alert(" Too large! Please input a number smaller than 100.");
   } else {
-    allSquares.forEach((square) => {
-      container.removeChild(square);
-    });
-    let newWidth = 960 / squareNum;
-    for (let i = 0; i < squareNum * squareNum; i++) {
-      let squareDiv = document.createElement("div");
-      squareDiv.style.width = `${newWidth}px`;
-      squareDiv.style.height = `${newWidth}px`;
-      container.appendChild(squareDiv);
-    }
-    allSquares = document.querySelectorAll(".container > div");
-    allSquares.forEach((square) => {
-      square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "#fb8500";
-      });
-      square.addEventListener("mouseleave", () => {
-        square.style.backgroundColor = "#219ebc";
-      });
-    });
+    container.replaceChildren();
   }
-});
+  for (let i = 0; i < squareNumPerSide * squareNumPerSide; i++) {
+    let squareDiv = document.createElement("div");
+    width = 960 / squareNumPerSide;
+    squareDiv.style.width = `${width}px`;
+    squareDiv.style.height = `${width}px`;
+    container.appendChild(squareDiv);
+  }
+}
